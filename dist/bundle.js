da845ccbcc68874675657fb72c5b17c9ef161775
Rohit Gupta <rohit.rewa@gmail.com>
2024-03-13 13:28:20 +0530

    dist

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+document.body.onload = function () {
+    alert("LOADED!");
+};
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsUUFBUSxDQUFDLElBQUksQ0FBQyxNQUFNLEdBQUc7SUFDbkIsS0FBSyxDQUFDLFNBQVMsQ0FBQyxDQUFDO0FBQ3JCLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wbHV0by8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJkb2N1bWVudC5ib2R5Lm9ubG9hZCA9IGZ1bmN0aW9uICgpIHtcclxuICAgIGFsZXJ0KFwiTE9BREVEIVwiKTtcclxufSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
